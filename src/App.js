e29733205b2c1d0ad752d4e9979abcb9257c1c1b
Matt Carlone <matthewcarlone@hey.com>
2020-09-09 16:15:51 -0400

    add initial form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,64 @@
 import React from 'react';
+import { useState } from "react";
+
 import logo from './logo.svg';
 import './App.css';
+
+// crocks
+import prop from 'crocks/Maybe/prop'; // prop returns a Maybe
+
+// helpers
+import liftA2 from "crocks/helpers/liftA2";
+
+
+
+// const { Just, Nothing } = Maybe;
+
+// const an = All(Nothing());
+// const aj = All(Just(1));
+// console.log( an.concat(aj).valueOf() );
+
+
+const FormFun = props => {
+
+  const [values, setValues] = useState({});
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+    // event.persist();
+    setValues({ ...values, [name]: value });
+  };
+
+  const maybeFirstName = prop('firstName', values);
+  const maybeLastName = prop('lastName', values);
+
+  const fullName = firstName => lastName => {
+    console.log('fullname call:', firstName, lastName)
+    return `${firstName} ${lastName}`;
+  };
+
+  console.log('hey', liftA2(fullName)(maybeFirstName)(maybeLastName));
+
+  return (
+    <form>
+      <label>
+        First Name:
+        { <input onChange={handleChange} value={maybeFirstName.option("")} type="text" name="firstName" /> }
+      </label>
+      <label>
+        Last Name:
+        { <input onChange={handleChange} value={maybeLastName.option("")} type="text" name="lastName" /> }
+      </label>
+      <div>
+        Fullname: { fullName(maybeFirstName.option(""), maybeLastName.option("")) }
+      </div>
+    </form>
+  );
+};
+
+
+
 
 function App() {
   return (
@@ -19,6 +77,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <FormFun/>
     </div>
   );
 }
